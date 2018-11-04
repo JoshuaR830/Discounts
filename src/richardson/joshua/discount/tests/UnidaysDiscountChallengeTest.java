b3af94b5bee88c516825c205ae56cf7f2a1cf5e1
Joshua <psyjr4@nottingham.ac.uk>
2018-11-04 14:28:54 +0000

    Added the test stubs

---
+++ src/richardson/joshua/discount/tests/UnidaysDiscountChallengeTest.java
@@ -0,0 +1,41 @@
+package richardson.joshua.discount.tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class UnidaysDiscountChallengeTest {
+
+    @Test
+    public void addToBasket() {
+
+    }
+
+    @Test
+    public void calculateTotalPriceA() {
+
+    }
+
+    @Test
+    public void calculateTotalPriceB() {
+
+    }
+
+    @Test
+    public void calculateTotalPriceC() {
+
+    }
+
+    @Test
+    public void calculateTotalPriceD() {
+
+    }
+
+    @Test
+    public void calculateTotalPriceE() {
+
+    }
+
+
+}
