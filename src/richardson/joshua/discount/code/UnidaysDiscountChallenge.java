a06f975ad0151bac4e2d56b01850d18c57e97b55
Joshua <psyjr4@nottingham.ac.uk>
2018-11-04 14:27:56 +0000

    Added the method stubs - AddToBasket, CalculateTotalPrice and some private ones that will be useful for calculating the values for each different item

---
+++ src/richardson/joshua/discount/code/UnidaysDiscountChallenge.java
@@ -0,0 +1,54 @@
+package richardson.joshua.discount.code;
+
+public class UnidaysDiscountChallenge {
+
+    public static void main(String[] args){
+        UnidaysDiscountChallenge discount = new UnidaysDiscountChallenge();
+
+    }
+
+    // Takes the items that the user enters
+    public void addToBasket(){
+
+    }
+
+    // Works out the total item price, delivery and total overall price
+    public void calculateTotalPrice(){
+
+    }
+
+    // Applies rules for A
+    private int calculateA(int num){
+
+        return 0;
+    }
+
+    // Applies rules for B
+    private int calculateB(int num){
+
+        return 0;
+    }
+
+    // Applies rules for C
+    private int calculateC(int num){
+
+        return 0;
+    }
+
+    // Applies rules for D
+    private int calculateD(int num){
+
+        return 0;
+    }
+
+    // Applies rules for E
+    private int calculateE(int num){
+
+        return 0;
+    }
+
+
+
+
+
+}
